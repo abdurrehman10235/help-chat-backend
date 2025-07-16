9f18952be648b7d78f517a65a3d761adb45c5b7a
abdurrehman10235 <107760550+abdurrehman10235@users.noreply.github.com>
2025-07-16 21:03:53 +0300

    index

--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,16 @@
 
 use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Facades\File;
+use Illuminate\Support\Facades\Response;
 
 Route::get('/{any}', function () {
-    return File::get(public_path('index.html'));
+    $file = public_path('index.html');
+
+    if (!File::exists($file)) {
+        abort(404);
+    }
+
+    return Response::make(File::get($file), 200, [
+        'Content-Type' => 'text/html',
+    ]);
 })->where('any', '.*');
