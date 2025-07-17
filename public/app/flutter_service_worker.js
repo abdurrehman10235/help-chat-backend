9f8f794b7f0b97041d1ba0251b28109feac191b7
abdurrehman10235 <107760550+abdurrehman10235@users.noreply.github.com>
2025-07-17 21:33:43 +0300

    web build

--- public/app/flutter_service_worker.js
+++ public/app/flutter_service_worker.js
@@ -3,11 +3,11 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"flutter_bootstrap.js": "1ae2aa46d2fb196242553d8a2a1aca2c",
+const RESOURCES = {"flutter_bootstrap.js": "41f033b33ccae352ba42c2b78425e36c",
 "version.json": "2b521e10dfa0f067561de489a19d6620",
 "index.html": "9484849b62453dae1815dac0f6295b68",
 "/": "9484849b62453dae1815dac0f6295b68",
-"main.dart.js": "c399a4ac7bb75db6375e22f7ecea2ad6",
+"main.dart.js": "cde63f7bf22e4739ea66475f284ae496",
 "flutter.js": "83d881c1dbb6d6bcd6b42e274605b69c",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
